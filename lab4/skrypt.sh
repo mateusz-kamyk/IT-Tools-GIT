a4f1588d8496d0c2fafeb4d9cacff15e53252654
mateusz-kamyk <mateusz_kamyk@outlook.com>
2024-07-21 11:41:35 +0200

    Revert "Added lab4 exercise solution for 1.3"
    This reverts commit f70b587d2bbf7d68f077af9a9cfc5464e3f964ce.

--- lab4/skrypt.sh
+++ lab4/skrypt.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
-ARGS=$1
+DATE=$1
 
-if [ "$ARGS" = "--date" ]
+if [ "$DATE" = "--date" ]
 then
 	date
-elif [ "$ARGS" = "--logs" ]
-then
-	n=100
-	for (( i = 1; i <= n; i++ )); do
-		touch "log${i}.txt"
-		echo "Filename: log${i}.txt, Created by script: skrypt.sh" >> log${i}.txt
-	        date >> log${i}.txt	
-	done
 fi
-	
-
-
-
 
