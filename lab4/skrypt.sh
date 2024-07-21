8602e5babeb959425428ff26e10f982e22767742
mateusz-kamyk <mateusz_kamyk@outlook.com>
2024-07-21 12:13:33 +0200

    Added lab4 exercise solution for 1.5

--- lab4/skrypt.sh
+++ lab4/skrypt.sh
@@ -3,6 +3,13 @@
 ARG1=$1
 ARG2=$2
 
+function print_help {
+	echo "Arguments:"
+	echo "--date       Print current date"
+	echo "--logs [n]   Make n logn.txt files"
+	echo "--help       Print help message"
+}
+
 if [ "$ARG1" = "--date" ]
 then
 	date
@@ -14,6 +21,12 @@ then
 		echo "Filename: log${i}.txt, Created by script: skrypt.sh" > log${i}.txt
 	        date >> log${i}.txt	
 	done
+elif [ "$ARG1" = "--help" ]
+then
+	print_help
+else
+	echo "You did not provide any valid argument"
+	print_help
 fi
 
 
